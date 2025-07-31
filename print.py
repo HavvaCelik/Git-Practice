86218e559e03361f0e1260acb5a0cb433195d179
Havva <havva@Havva-MacBook-Air-2.local>
2025-07-31 23:21:53 +0300

    Add function has been added

--- print.py
+++ print.py
@@ -1,1 +1,7 @@
-print("Hello github, I'am Havva")
+def add_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+print("Add function has been added.")
+result = add_numbers(3, 5)
+print("Sum:", result)
